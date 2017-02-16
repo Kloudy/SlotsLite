18cf9baa3d95f6f10d4ccf0b4f363c55507dc90a
Kloudy <kloudy@antarescraft.com>
2017-02-15 17:04:25 -0700

    -

---
+++ src/com/antarescraft/kloudy/slotslite/SlotsLite.java
@@ -0,0 +1,18 @@
+package com.antarescraft.kloudy.slotslite;
+
+import com.antarescraft.kloudy.hologuiapi.HoloGUIPlugin;
+
+public class SlotsLite extends HoloGUIPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+}
