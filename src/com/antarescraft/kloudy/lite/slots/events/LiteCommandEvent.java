80a07d96aac497f9b5f0ad914cc5905502c04027
Kloudy <kloudy@antarescraft.com>
2017-02-20 18:30:29 -0700

    -

---
+++ src/com/antarescraft/kloudy/lite/slots/events/LiteCommandEvent.java
@@ -0,0 +1,13 @@
+package com.antarescraft.kloudy.lite.slots.events;
+
+import com.antarescraft.kloudy.slots.Slots;
+import com.antarescraft.kloudy.slots.events.CommandEvent;
+
+public class LiteCommandEvent extends CommandEvent
+{
+
+	public LiteCommandEvent(Slots plugin)
+	{
+		super(plugin);
+	}
+}
--- src/com/antarescraft/kloudy/slotslite/SlotsLite.java
+++ src/com/antarescraft/kloudy/slotslite/SlotsLite.java
@@ -1,8 +1,13 @@
 package com.antarescraft.kloudy.slotslite;
 
 import com.antarescraft.kloudy.slots.Slots;
+import com.antarescraft.kloudy.slots.events.CommandEvent;
 
 public class SlotsLite extends Slots
 {
-	
+	@Override
+	public void onEnable()
+	{
+		getCommand("slots").setExecutor(new CommandEvent(this));
+	}
 }
