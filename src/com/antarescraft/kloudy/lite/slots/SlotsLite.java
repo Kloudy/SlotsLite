470be86626100c53aff3494cb62dbadaa9389dab
Kloudy <kloudy@antarescraft.com>
2017-02-22 20:02:38 -0700

    -

--- src/com/antarescraft/kloudy/lite/slots/SlotsLite.java
+++ src/com/antarescraft/kloudy/lite/slots/SlotsLite.java
@@ -1,13 +1,15 @@
 package com.antarescraft.kloudy.lite.slots;
 
+import com.antarescraft.kloudy.lite.slots.events.LiteCommandEvent;
 import com.antarescraft.kloudy.slots.Slots;
-import com.antarescraft.kloudy.slots.events.CommandEvent;
 
 public class SlotsLite extends Slots
 {
 	@Override
 	public void onEnable()
 	{
-		getCommand("slots").setExecutor(new CommandEvent(this));
+		getCommand("slotslite").setExecutor(new LiteCommandEvent(this));
+		
+		super.onEnable();
 	}
 }
--- src/com/antarescraft/kloudy/lite/slots/events/LiteCommandEvent.java
+++ src/com/antarescraft/kloudy/lite/slots/events/LiteCommandEvent.java
@@ -1,11 +1,15 @@
 package com.antarescraft.kloudy.lite.slots.events;
 
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.antarescraft.kloudy.hologuiapi.plugincore.command.CommandHandler;
+import com.antarescraft.kloudy.hologuiapi.plugincore.command.CommandParser;
+import com.antarescraft.kloudy.promo.HoloGUIPromo;
 import com.antarescraft.kloudy.slots.Slots;
 import com.antarescraft.kloudy.slots.events.CommandEvent;
+import com.antarescraft.kloudy.slots.pagemodels.SlotsPageModel;
 
 public class LiteCommandEvent extends CommandEvent
 {
@@ -14,15 +18,22 @@ public class LiteCommandEvent extends CommandEvent
 		super(plugin);
 	}
 	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		return CommandParser.parseCommand(slots, this, "slotslite", cmd.getName(), sender, args);
+	}
+	
+	@Override
 	@CommandHandler(description = "Opens the Slots GUI",
 			mustBePlayer = true, permission = "slots.play", subcommands = "play")
 	public void play(CommandSender sender, String[] args)
 	{
 		Player player = (Player)sender;
-		
+				
 		if(!slots.isPlaying(player))
 		{
-			
+			HoloGUIPromo.openPromoPage(slots, player, new SlotsPageModel(slots, slots.getGUIPage("slot-machine"), player));
 		}
 		else
 		{
