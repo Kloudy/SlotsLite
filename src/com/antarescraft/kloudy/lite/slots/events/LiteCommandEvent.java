ccea3c04e0513760a0d30a1e662decb300816ceb
Kloudy <kloudy@antarescraft.com>
2017-02-26 09:23:38 -0700

    v1.1

--- src/com/antarescraft/kloudy/lite/slots/events/LiteCommandEvent.java
+++ src/com/antarescraft/kloudy/lite/slots/events/LiteCommandEvent.java
@@ -6,8 +6,10 @@ import org.bukkit.entity.Player;
 
 import com.antarescraft.kloudy.hologuiapi.plugincore.command.CommandHandler;
 import com.antarescraft.kloudy.hologuiapi.plugincore.command.CommandParser;
+import com.antarescraft.kloudy.hologuiapi.plugincore.messaging.MessageManager;
 import com.antarescraft.kloudy.promo.HoloGUIPromo;
 import com.antarescraft.kloudy.slots.Slots;
+import com.antarescraft.kloudy.slots.SlotsConfiguration;
 import com.antarescraft.kloudy.slots.events.CommandEvent;
 import com.antarescraft.kloudy.slots.pagemodels.SlotsPageModel;
 
@@ -25,12 +27,26 @@ public class LiteCommandEvent extends CommandEvent
 	}
 	
 	@Override
+	@CommandHandler(description = "Reloads the config files", 
+			mustBePlayer = false, permission = "slotslite.admin", subcommands = "reload")
+	public void reload(CommandSender sender, String[] args)
+	{
+		slots.getHoloGUIApi().destroyGUIPages(slots);
+
+		slots.removeAllPlayers();
+		
+		SlotsConfiguration.loadConfig(slots);
+		
+		MessageManager.info(sender, "Reloaded the config");
+	}
+	
+	@Override
 	@CommandHandler(description = "Opens the Slots GUI",
 			mustBePlayer = true, permission = "slotslite.play", subcommands = "play")
 	public void play(CommandSender sender, String[] args)
 	{
 		Player player = (Player)sender;
-				
+						
 		if(!slots.isPlaying(player))
 		{
 			HoloGUIPromo.openPromoPage(slots, player, "https://www.spigotmc.org/members/kloudy.42303/",
